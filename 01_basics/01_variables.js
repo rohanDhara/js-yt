be0652703b8a15ddf34a4c824a39e44a467cdb46
Rohan30 <90055733+rohanDhara@users.noreply.github.com>
2024-02-17 16:21:57 +0000

    datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=125412;
+let accountEmail="rohan@google.com";
+var accountPassword="12345"
+accountLocation="Maharastra"
+let accountType;
+
+//accountId=999777; // assignment to constant variable is not allowed
+console.log(accountId);
+
+console.table([accountId , accountEmail, accountPassword, accountLocation,accountType]);
+/*
+    try to avoid using var datatype beacuse of block scope or function scope
+*/
