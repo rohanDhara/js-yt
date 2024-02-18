0357e0b35903713dbec01f7a2c60936473f3f05d
rohan30 <dhara.rohan132@gmail.com>
2024-02-18 17:50:47 +0530

    type conversion done

---
+++ 01_basics/03_conversionOfDatatypes.js
@@ -0,0 +1,19 @@
+let marks="40";
+
+//console.log(marks);
+//console.log(typeof marks);
+
+let convertedMarks = Number(marks);
+//console.log(typeof convertedMarks);
+//console.log(convertedMarks);
+
+let myName= null;
+console.log(typeof myName);
+let convertedName = Boolean(myName);
+console.log(typeof convertedName);
+console.log(Number(myName));
+
+
+//null=>in String or number gives 0
+//String -> Number => NaN
+//"" => false
