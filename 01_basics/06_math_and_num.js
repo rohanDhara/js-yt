1287d623a4775f790b00ecba0cf00c22f4ccd748
rohan30 <dhara.rohan132@gmail.com>
2024-02-19 23:49:26 +0530

    playing with numbers ans Math functions

---
+++ 01_basics/06_math_and_num.js
@@ -0,0 +1,21 @@
+const mohunBagan = 3.6821;
+// console.log(mohunBagan.toPrecision(2));
+const modifiedMohunBagan = new Number(30);
+// console.log(modifiedMohunBagan);
+
+// **********Math*********
+
+// console.log(Math.abs(-3.65));
+// console.log(Math.ceil(5.32));
+// console.log(Math.floor(5.34));
+// console.log(Math.round(13.67));
+// console.log(Math.max(12,34,67,11,90));
+// console.log(Math.min(67,89,45,38,82,17,32));
+
+// console.log(Math.random()); //returns value from 0 - 1
+// console.log((Math.random()*10)+1); //if it gives a value 0.00 then by adding 1 we can get value from 1 
+
+const min=10;
+const max = 30;
+
+console.log(Math.floor((Math.random())*(max-min+1)+min)); //remember this formula
