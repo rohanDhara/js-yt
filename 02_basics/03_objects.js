85185284ae7729f0b97a52b6e3445d6fb9c609cf
rohan30 <dhara.rohan132@gmail.com>
2024-02-20 23:56:55 +0530

    objects first part done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//we can create object of two types 1.literals 2.singleton
+//singleton objects are related with constructors
+//singleton
+//Object.create (constructor method to create objects)
+
+//literals in objects data are stored in a key value format
+const myData =
+{
+    name:"rohan",
+    age:22,
+    isGraduate:false,
+    location:"Madhyamgram",
+    email:"rohan@mail.go",
+    holdings:["land","house"]   
+}
+console.log(myData);
