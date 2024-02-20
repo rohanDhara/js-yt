9146d7673349f2f291c06e3abfb57bb47fb6194d
rohan30 <dhara.rohan132@gmail.com>
2024-02-20 22:21:13 +0530

    array part one done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+//Declaration, definition and accessing of array
+
+const myArr = [0,11,12,31,41,13];
+const cars = [14, true,"Bmw"]; //we can store different datatypes in same array
+// console.log(cars);
+// console.log(myArr[4]);
+//console.log(typeof myArr);
+const myArr2 = new Array(1,2,3,4);
+// console.log(myArr2);
+
+//functions of Array
+
+const myHero=new Array(12,32,45,41,15,22);
+// console.log(myHero);
+myHero.push(42);
+// console.log(myHero);
+myHero.pop()
+// console.log(myHero);
+
+myHero.splice(1,3)
+console.log(myHero);
+
