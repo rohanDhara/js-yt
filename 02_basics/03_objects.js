31e1ae6a1b29b1726ee943543e4ca638df2a1426
rohan30 <dhara.rohan132@gmail.com>
2024-02-21 00:58:16 +0530

    objects modified

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,13 +4,36 @@
 //Object.create (constructor method to create objects)
 
 //literals in objects data are stored in a key value format
+const mySm=Symbol("Key1");
 const myData =
 {
     name:"rohan",
     age:22,
+    "full Name":"Rohan Dhara",
     isGraduate:false,
+    [mySm]:"key2",//specified that it is a symbol
     location:"Madhyamgram",
     email:"rohan@mail.go",
     holdings:["land","house"]   
 }
-console.log(myData);
+// console.log(myData);
+// console.log(myData.age);
+// console.log(myData["location"]); //we can also access data like this 
+// console.log(myData[mySm]);//access symbol like this
+// console.log(typeof myData[mySm]); 
+
+myData.email="goo@bool.com"
+// console.log(myData.email);
+//Object.freeze(myData); //freeze made the values freeze we cannot modify it
+
+myData.email="@microsoft.com"
+// console.log(myData.email);
+
+myData.greetings = function greeting()
+{
+    console.log("Hello from Rohan");
+}
+myData.greetingsTwo = function greetingTwo()
+{
+    console.log(`hello from ${myData.name}`); //Error in here fix it before go to next video
+}
