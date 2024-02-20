ab5f70b72d1aab6939e66944c86956dfa7d4b5c0
rohan30 <dhara.rohan132@gmail.com>
2024-02-20 21:48:56 +0530

    dates updated

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,5 +1,20 @@
 const today = new Date();
-console.log(today);
-console.log(today.toString());
-console.log(today.toLocaleString());
+// console.log(today);
+// console.log(today.toString());
+// console.log(today.toLocaleString());
+// console.log(today.toDateString());
+//console.log(today.toLocaleDateString());
+
+//Time
+const myTime = Date.now();
+// console.log(myTime);
+// console.log(Math.floor(myTime)/1000);
+
+// console.log(today.getDate());
+// console.log(today.getDay());
+console.log(today.getTime());
+
+myTime.toLocaleString('default',
+    //here we can define 
+)
 
