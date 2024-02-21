f876027be0859018fc1cc3b157bb5a5a303d62bb
rohan30 <dhara.rohan132@gmail.com>
2024-02-21 23:57:47 +0530

    hoisting and scope done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,40 @@
+//var a = 3000
+let b=5000
 
+if (true) {
+    var a = 30;
+    let b = 50;
+    const c = 70;
+    // console.log("Inner b "+b);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+function first()
+{
+    const user = "Rohan";
+    function second() {
+        const insideSecond = "I am inside second"
+        console.log(user+" "+insideSecond);
+    }
+   // console.log(insideSecond);
+    //second()
+}
+first()
 
+//hoisting in javascript
 
+console.log(fun1(6))
 
+function fun1(num)
+{
+    return num+1
+}
 
-
-
-
-
-
-
-
+console.log(holding(10));
+const holding = function(num) // varible is declared here we cannot access it before its declaration
+{
+    return num+2
+}
 
