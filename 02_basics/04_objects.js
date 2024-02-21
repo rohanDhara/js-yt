06cbc885296c4397cfe95827cb073c1bfab4a500
rohan30 <dhara.rohan132@gmail.com>
2024-02-21 21:21:46 +0530

    objects modified

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,4 +30,38 @@ const obj2 = {3: "a", 4: "b"};
 let obj3 = {obj1,obj2}; // it creates objects inside an object
 // obj3 = Object.assign({},obj1,obj2); //assings all the object values to the first object in parameter
 obj3={...obj1,...obj2};
-console.log(obj3);
+// console.log(obj3);
+
+
+ const users = 
+ [
+    {
+        name:"Rohan"
+    },
+    {
+        ages:45
+    },
+    {
+        status:"login"
+    }
+]
+//  console.log(users[0].name);
+
+// console.log(Object.keys(obj2));
+// console.log(Object.values(obj2));
+// console.log(Object.entries(obj2)); //retuens objetcs inside and array
+
+// console.log(obj1.hasOwnProperty("1"));
+
+//object de-structure and JSON API
+
+const college = {
+    collegeName:"GNIT",
+    address:"Sodepur",
+    contact:33-9854-65412
+}
+
+college.collegeName;//we will access like that but 
+
+const {collegeName: name} = college  //object de-structuring
+console.log(name);
