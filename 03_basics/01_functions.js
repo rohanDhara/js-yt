2179067169260ae0ecf37b379d641062e371213f
rohan30 <dhara.rohan132@gmail.com>
2024-02-21 22:06:29 +0530

    functions partially done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+
+function myName()
+{
+    console.log("R");
+    console.log("o");
+    console.log("h");
+    console.log("a");
+    console.log("n");
+}
+//yName //this is reference
+// console.log(myName()); // this is call
+function addMoreTwoNums(num1,num2) //in parameter we doesn't have to write data type
+{
+    // let result = num1+num2;
+    // return result;
+}
+
+function addTwoNums(num1,num2) //in parameter we doesn't have to write data type
+{
+    return num1+num2
+}
+const nums=addTwoNums(3,5);
+console.log("Rohan :"+nums);
