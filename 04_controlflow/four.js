259f221393379cdd7adbcd0b496ff0b5e46a47a3
rohan30 <dhara.rohan132@gmail.com>
2024-02-24 22:48:23 +0530

    filter map reduce added

--- 04_controlflow/four.js
+++ 04_controlflow/four.js
@@ -1,6 +1,17 @@
 
 const car = ["BMW","Audi","Marcedes","Mclaren","Lamborgini"]
 
-car.forEach( (item) => {
-    console.log(`${item}`);
-} )
+// const getValue=car.forEach( (item) => {
+//     //console.log(`${item}`);
+//     return item;
+// } )                                  forEach functions do not return anything
+// console.log(getValue);
+
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
+const getVal=nums.filter( (val) => {val > 5} ) //fliter returns values,here this is implicit return
+// const ge
+tVal=nums.filter( (val) => {val > 5} ) //this will return an empty object beacuse {} for this block scope we have to give explixit return
+console.log(getVal);
+
+// nums.forEach( () )
