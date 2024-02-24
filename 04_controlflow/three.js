9c77c4088764086a473fbd848740b9f21826d804
rohan30 <dhara.rohan132@gmail.com>
2024-02-24 21:55:11 +0530

    forEach for objects in array

---
+++ 04_controlflow/three.js
@@ -0,0 +1,34 @@
+const lang =["c","java","js","c++","python"];
+
+lang.forEach(function (item) {
+    //console.log(item); //call back function
+})
+
+lang.forEach((val) => {
+    // console.log(val);
+})
+
+function printable(val)
+{
+    // console.log(val);
+}
+lang.forEach(printable); //here we have to pass ta reference of the function
+
+const languages = [
+    {
+        languageName:"JavaScript",
+        languageExtension : ".js"
+    },
+    {
+        languageName:"Java",
+        languageExtension : ".java"
+    },
+    {
+        languageName:"Python",
+        languageExtension : ".py"
+    }
+] 
+
+languages.forEach( (item) => {
+    console.log(`${item.languageExtension} for ${item.languageName}`);
+})
