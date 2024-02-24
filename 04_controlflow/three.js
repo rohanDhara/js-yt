2686ceb547351d15189e82c29ead147029faf8dc
rohan30 <dhara.rohan132@gmail.com>
2024-02-24 21:59:02 +0530

    foreach modified

--- 04_controlflow/three.js
+++ 04_controlflow/three.js
@@ -1,18 +1,22 @@
 const lang =["c","java","js","c++","python"];
 
 lang.forEach(function (item) {
-    //console.log(item); //call back function
+    console.log(item); //call back function
 })
 
 lang.forEach((val) => {
-    // console.log(val);
+     console.log(val);
 })
 
 function printable(val)
 {
-    // console.log(val);
+     console.log(val);
 }
-lang.forEach(printable); //here we have to pass ta reference of the function
+lang.forEach(printable);//here we have to pass ta reference of the function
+
+lang.forEach( (item,index,arr) => {
+     console.log(item,index,arr);
+} )
 
 const languages = [
     {
@@ -30,5 +34,5 @@ const languages = [
 ] 
 
 languages.forEach( (item) => {
-    console.log(`${item.languageExtension} for ${item.languageName}`);
+     console.log(`${item.languageExtension} for ${item.languageName}`);
 })
