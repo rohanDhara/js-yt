900c3b51eefa1b3a29f4d4f37cdbb6223b6c41e0
rohan30 <dhara.rohan132@gmail.com>
2024-02-27 00:01:39 +0530

    color changer

--- Projects/01_colorChanger/script.js
+++ Projects/01_colorChanger/script.js
@@ -20,6 +20,21 @@ button.forEach((color) => {
             case 'chartreuse':
                 body.style.backgroundColor = e.target.id;
                 break;
+            case 'sienna':
+                body.style.background = e.target.id
+                break;
+            case 'firebrick':
+                body.style.background = e.target.id
+                break;
+            case 'midnightblue':
+                body.style.background = e.target.id
+                break;
+            case 'palegoldenrod':
+                body.style.background = e.target.id
+                break;
+            case 'teal':
+                body.style.background = e.target.id
+                break;
             default:
                 body.style.backgroundColor = black;                       
         }
