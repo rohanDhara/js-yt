4971ba152f17b55942816c09e7ca952f6fd1cce5
rohan30 <dhara.rohan132@gmail.com>
2024-03-07 23:39:12 +0530

    getter and setter

--- 08_classes_and_oop/getter_setter.js
+++ 08_classes_and_oop/getter_setter.js
@@ -4,14 +4,20 @@ class User{
         this.email = email;
         this.password = password;
     }
-
+    // if we use getR then we have to use setR and setR will always accept one parameter, which is to be set
+    get email(){
+        return `${this._email.toUpperCase()}`
+    }
+    set email(e){
+        this._email = e;
+    }
     get password(){
-        return this.password.toUpperCase();
+        return `${this._password}rohan` // we always send password as like this in encryption format, don't directly send password
     }
     set password(p){
-        
+        this._password = p;
     }
 }
 
-const rohan = new User("r@dhara.com","123");
-console.log(rohan.password);
+const rohan = new User("r@dhara.com","abc");
+console.log(rohan.email);
